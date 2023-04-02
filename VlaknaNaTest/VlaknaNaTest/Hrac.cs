f28861106405495e71b6e888967b89830db9446c
Jiří Syrovátko <113086098+JirkS@users.noreply.github.com>
2023-04-02 19:18:25 +0200

    Add files via upload

---
+++ VlaknaNaTest/VlaknaNaTest/Hrac.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VlaknaNaTest
+{
+    public class Hrac
+    {
+
+        private string name;
+        private int health;
+        private bool isAlive;
+        private int max;
+        private Object playerLock;
+
+        public Hrac(string name, int max)
+        {
+            Name = name;
+            Health = max;
+            IsAlive = true;
+            Max = max;
+            PlayerLock = new object();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public bool IsAlive
+        {
+            get { return isAlive; }
+            set { isAlive = value; }
+        }
+
+        public int Max
+        {
+            get { return max; }
+            set { max = value; }
+        }
+
+        public Object PlayerLock
+        {
+            get { return playerLock; }
+            set { playerLock = value; }
+        }
+
+        public void Heal(int value)
+        {
+            lock (playerLock)
+            {
+                if (isAlive)
+                {
+                    health += value;
+                    if(health > max)
+                    {
+                        health = max;
+                    }
+                    Console.WriteLine($"Player: {name} heal {value}hp, hp = {health}");
+                }
+                else
+                {
+                    Console.WriteLine($"Player: {name} heal {value}hp is dead! hp = {health}");
+                }
+            }
+        }
+
+        public void Damage(int value)
+        {
+            lock (playerLock)
+            {
+                if (isAlive)
+                {
+                    health -= value;
+                    if (health <= 0)
+                    {
+                        isAlive = false;
+                        health = 0;
+                        Console.WriteLine($"Player: {name} damage {value}hp is dead! hp = {health}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Player: {name} damage {value}hp, hp = {health}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Player: {name} is already dead! hp = {health}");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{name}, HP {health} player is alive: {isAlive}";
+        }
+
+    }
+}
---
+++ VlaknaNaTest/VlaknaNaTest/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace VlaknaNaTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gnr = new Random();
+            Hrac hrac = new Hrac("IronMan", 100);
+
+            Thread t1 = new Thread(() => Damage(hrac, gnr));
+            Thread t2 = new Thread(() => Heal(hrac, gnr));
+
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            Console.WriteLine(hrac.ToString());
+
+        }
+        public static void Heal(Hrac h, Random gnr)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                h.Heal(gnr.Next(10) + 1);
+            }
+        }
+
+        public static void Damage(Hrac h, Random gnr)
+        {
+
+            for(int i = 0; i < 10; i++)
+            {
+                h.Damage(gnr.Next(10)+1);
+            }
+        }
+
+    }
+}
